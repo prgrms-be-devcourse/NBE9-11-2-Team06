6566f17e00f40148816a86ccb30fef5bfcc07738
ppinggu9 <pping5644@gmail.com>
2026-04-13 14:14:33 +0900

    init

---
+++ src/main/java/com/back/nbe9112team06/Nbe9112Team06Application.java
@@ -0,0 +1,13 @@
+package com.back.nbe9112team06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe9112Team06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe9112Team06Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/nbe9112team06/Nbe9112Team06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.nbe9112team06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe9112Team06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
