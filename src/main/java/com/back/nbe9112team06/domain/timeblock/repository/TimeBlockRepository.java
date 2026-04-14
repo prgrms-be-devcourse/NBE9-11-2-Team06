c0103cc63234978e0e5ffc8da0f6ff32999b5d17
parkJJae <wgpark0830@naver.com>
2026-04-14 14:21:20 +0900

    feat: TimeBlock 엔티티 및 레파지토리 생성

---
+++ src/main/java/com/back/nbe9112team06/domain/timeblock/repository/TimeBlockRepository.java
@@ -0,0 +1,14 @@
+package com.back.nbe9112team06.domain.timeblock.repository;
+
+import com.back.nbe9112team06.domain.participant.entity.Participant;
+import com.back.nbe9112team06.domain.timeblock.entity.TimeBlock;
+import com.back.nbe9112team06.domain.meeting.entity.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TimeBlockRepository extends JpaRepository<TimeBlock, Long> {
+
+    // 중복등록체크 (같은 모임에 같은 이름으로 이미 등록했는지)
+    Optional<TimeBlock> findByMeetingAndParticipantName(Meeting meeting, Participant participant);
+}
