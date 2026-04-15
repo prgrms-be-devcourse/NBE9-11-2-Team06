081ad766febc315af16844841e846b2de568561f
ppinggu9 <pping5644@gmail.com>
2026-04-16 01:12:28 +0900

    글로벌예외 핸들러 + devtool종속성 + application.yaml 및 test ãdev 수정+ user -> member + springsecurity임시설정
    # Conflicts:
    #	src/main/java/com/back/nbe9112team06/domain/meeting/entity/Meeting.java
    #	src/main/java/com/back/nbe9112team06/domain/member/entity/TimezoneType.java
    #	src/main/java/com/back/nbe9112team06/global/security/SecurityConfig.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
     compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
